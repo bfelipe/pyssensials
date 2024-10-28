c699357c3ea94bc498cecd9d5787340186ef7ce4
Bruno Felipe <cs.felipebm@gmail.com>
2024-10-28 16:05:23 -0300

    add Matrix data structure into graph module

--- graph.py
+++ graph.py
@@ -1,72 +1,115 @@
-class Vertex:
+from typing import Any, List, Tuple, Dict
 
-    def __init__(self, key):
-        self.key = key
-        self.neighbors = {}
-        self.connections = 0
+class Matrix:
+    
+    def __init__(self, dimension:Tuple[int,int], fill:Any=0):
+        self.rows = dimension[0]
+        self.cols = dimension[1]
+        self.fill = fill
+        self.grid = [[self.fill] * self.cols for _ in range(self.rows)]
 
-    def add_neighbor(self, vertex, weight=None):
-        if vertex in self.neighbors:
-            self.neighbors[vertex] = weight
-        else:
-            self.neighbors[vertex] = weight
-            self.connections += 1
+    def add_vertexes(self, value:Any, coordinates:List[Tuple[int, int]]) -> None:
+        for coord in coordinates:
+            r,c = coord
+            self.grid[r][c] = value
 
-    def get_connections(self):
-        return self.neighbors.keys(), self.connections
+    def dimension(self) -> Tuple[int,int]:
+        return (self.rows, self.cols)
+    
+    def vertex(self, coordinate:Tuple[int,int]) -> Any:
+        r, c = coordinate
+        return self.grid[r][c]
+    
+    def num_paths(self, start:Tuple[int,int], end:Tuple[int,int], blocked:List[Any]) -> int:
+        if (min(start) < 0 or max(start) >= max(self.dimension()) or 
+        min(end) < 0 or max(end) >= max(self.dimension())):
+            return -1
+        
+        paths = 0
+        visited = set()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-    def get_weight(self, vertex):
-        return self.neighbors.get(vertex)
+        def dfs(coordinate:Tuple[int, int]):
+            out = 0
+            r, c = coordinate
+            if (r not in range(self.rows) or c not in range(self.cols) or
+                coordinate in visited or self.vertex(coordinate) in blocked):
+                return out
+            
+            if (r, c) == end:
+                return 1
 
-    def disconnect(self, vertex):
-        if vertex in self.neighbors:
-            del self.neighbors[vertex]
-            self.connections -= 1
+            visited.add(coordinate)    
+            for dr, dc in directions:
+                out += dfs((r + dr, c + dc))
+            visited.remove(coordinate)
+            return out
+        
+        paths += dfs(start)
+        return paths
+    
+    def groups(self, target:Any) -> int:
+        count = 0
+        visited = set()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        def dfs(coordinate:Tuple[int, int]):
+            r, c = coordinate
+            if (min(coordinate) < 0 or max(coordinate) >= max(self.dimension()) or
+                coordinate in visited or self.vertex(coordinate) != target):
+                return
 
-    def __repr__(self):
-        return '<Vertex key: %s, neighbors: %s: connections: %s>' % (self.key,
-                                                                     self.neighbors,
-                                                                     self.connections)
+            visited.add(coordinate)
+            for dr, dc in directions:
+                dfs((r + dr, c + dc))
+
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if (row, col) not in visited and self.vertex((row, col)) == target:
+                    count += 1
+                    dfs((row, col))
+        return count
+    
+    def short_distance(self, start:Tuple[int, int], end:Tuple[int,int], blocked:List[Any]) -> int:
+        if (min(start) < 0 or max(start) >= max(self.dimension()) or 
+        min(end) < 0 or max(end) >= max(self.dimension())):
+            return -1
+
+        distance = 0
+        queue = []
+        visited = set()
+        queue.append(start)
+        visited.add(start)
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def bfs(coordinate:Tuple[int, int]):
+            if (min(coordinate) < 0 or max(coordinate) >= max(self.dimension()) or
+            coordinate in visited or self.vertex(coordinate) in blocked):
+                return
+            queue.append(coordinate)
+            visited.add(coordinate)
 
 
-class AdjacentListGraph:
+        while queue:
+            for _ in range(len(queue)):
+                row, col = queue.pop(0)
+                if (row, col) == end:
+                    return distance
+                
+                for dr, dc in directions:
+                    bfs((row + dr, col + dc))
+            distance += 1
+        return -1
 
+    def describe(self) -> None:
+        for r in self.grid:
+            print(r)
+
+    def __repr__(self) -> str:
+        return '<Matrix dimmension: %s, grid: %s>' % (self.dimension(), self.grid)
+    
+
+class AdjacentList:
+    
     def __init__(self):
-        self.vertices = {}
-        self.current_size = 0
-
-    def add_vertex(self, vertex: Vertex):
-        self.vertices[vertex.key] = vertex
-        self.current_size += 1
-
-    def add_edge(self, from_vtx, to_vtx, weight=None):
-        if from_vtx not in self.vertices:
-            self.add_vertex(Vertex(from_vtx))
-        if to_vtx not in self.vertices:
-            self.add_vertex(Vertex(to_vtx))
-        self.vertices[from_vtx].add_neighbor(to_vtx, weight)
-
-    def get_vertex_by(self, key):
-        return self.vertices.get(key)
-
-    def get_vertices(self):
-        return list(self.vertices.keys())
-
-    def __contains__(self, k):
-        return k in self.vertices
-
-    def __iter__(self):
-        return iter(self.vertices.values())
-
-    def is_empty(self):
-        return self.vertices == {}
-
-    def size(self):
-        return self.current_size
-
-    def clear(self):
-        self.vertices = {}
-        self.current_size = 0
-
-    def __repr__(self):
-        return '<Graph vertices: %s>' % self.vertices
+        self.vertices: Dict[int,List[int]]
--- test_graph.py
+++ test_graph.py
@@ -2,120 +2,31 @@ import unittest
 
 import graph
 
-
 class GraphTestCase(unittest.TestCase):
 
-    def test_Vertex(self):
-        vtx = graph.Vertex('a')
-        assert type(vtx) is graph.Vertex
-        assert vtx.key == 'a'
-        assert vtx.neighbors == {}
-        assert vtx.connections == 0
+    def test_Matrix(self):
+        m = graph.Matrix(dimension=(4, 4))
+        assert type(m) is graph.Matrix
+        assert m.dimension() == (4, 4)
+        for row in m.grid:
+            for col in row:
+                assert col == 0
+        assert m.fill == 0
 
-        vtx.add_neighbor('b')
-        vtx.add_neighbor('c', 2)
-
-        assert vtx.connections == 2
-        n, c = vtx.get_connections()
-        for key in n:
-            assert key in ['b', 'c']
-        assert c == 2
-
-        assert vtx.get_weight('b') is None
-        assert vtx.get_weight('c') == 2
-        assert vtx.get_weight('d') is None
-
-        vtx.disconnect('d')
-        assert vtx.connections == 2
-        vtx.disconnect('b')
-        assert vtx.connections == 1
-        n, c = vtx.get_connections()
-        assert 'b' not in n
-
-        vtx.add_neighbor('c', 3)
-        assert vtx.connections == 1
-        assert vtx.get_weight('c') == 3
-
-    def test_AdjacentListGraph(self):
-        alg = graph.AdjacentListGraph()
-        assert type(alg) is graph.AdjacentListGraph
-        assert alg.is_empty() is True
-        assert alg.size() == 0
-        assert alg.get_vertices() == []
-
-        for i in range(6):
-            alg.add_vertex(graph.Vertex(i))
-
-        alg.add_edge(0, 1, 5)
-        alg.add_edge(0, 5, 2)
-        alg.add_edge(1, 2, 4)
-        alg.add_edge(2, 3, 9)
-        alg.add_edge(3, 4, 7)
-        alg.add_edge(3, 5, 3)
-        alg.add_edge(4, 0, 1)
-        alg.add_edge(5, 4, 8)
-        alg.add_edge(5, 2, 1)
-
-        assert alg.is_empty() is False
-        assert alg.size() == 6
-
-        assert alg.get_vertex_by(6) is None
-        v = alg.get_vertex_by(0)
-        assert v.key == 0
-        assert v.neighbors == {1: 5, 5: 2}
-        assert v.connections == 2
-
-        v = alg.get_vertex_by(1)
-        assert v.key == 1
-        assert v.neighbors == {2: 4}
-        assert v.connections == 1
-
-        v = alg.get_vertex_by(2)
-        assert v.key == 2
-        assert v.neighbors == {3: 9}
-        assert v.connections == 1
-
-        v = alg.get_vertex_by(3)
-        assert v.key == 3
-        assert v.neighbors == {4: 7, 5: 3}
-        assert v.connections == 2
-
-        v = alg.get_vertex_by(4)
-        assert v.key == 4
-        assert v.neighbors == {0: 1}
-        assert v.connections == 1
-
-        v = alg.get_vertex_by(5)
-        assert v.key == 5
-        assert v.neighbors == {4: 8, 2: 1}
-        assert v.connections == 2
-
-        assert alg.get_vertices() == [k for k in range(6)]
-
-        alg.clear()
-        assert alg.is_empty() is True
-        assert alg.size() == 0
-        assert alg.get_vertices() == []
-
-    def test_BFS(self):
-        word_table = {}
-        alg = graph.AdjacentListGraph()
-
-        with open('words.txt', 'r') as file:
-
-            for line in file:
-                word = line[:-1]
-                for idx in range(len(word)):
-                    bucket = f'{word[:idx]}_{word[idx + 1:]}'
-                    if bucket in word_table:
-                        word_table[bucket].append(word)
-                    else:
-                        word_table[bucket] = [word]
-
-            for bucket in word_table.keys():
-                for w1 in word_table[bucket]:
-                    for w2 in word_table[bucket]:
-                        if w1 != w2:
-                            alg.add_edge(w1, w2)
-
-        word_table.clear()
+        coordinates = [(1, 0), (1, 1), (2, 3), (3, 1)]
+        m.add_vertexes(value=1, coordinates=coordinates)
+        for c in coordinates:
+            assert m.vertex(c) == 1
+        assert m.grid == [
+            [0, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 1],
+            [0, 1, 0, 0]]
+        
+        assert m.num_paths(start=(0,0), end=(3,3), blocked=[1]) == 2
+        assert m.num_paths(start=(0,0), end=(4,3), blocked=[1]) == -1
+        assert m.num_paths(start=(1,2), end=(2,2), blocked=[1]) == 1
+        assert m.groups(1) == 3
+        assert m.groups(0) == 1
+        assert m.short_distance(start=(0,0), end=(3,3), blocked=[1]) == 6
+        assert m.short_distance(start=(0,0), end=(4,3), blocked=[1]) == -1
